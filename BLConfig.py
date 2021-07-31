8768a3e3ad0e8fd2744e17ae08dc8e21a41aafb9
gloryapedia <85855318+gloryapedia@users.noreply.github.com>
2021-07-31 22:51:47 +0700

    Add files via upload

---
+++ BLConfig.py
@@ -0,0 +1,97 @@
+# Cek sampai bawah!!
+Basic   ={
+        "Username"             : "jantanzxz",
+        "Password"             : "Pejantan17#",
+        "BL-Key"               : "xxxxxxxxx",
+
+        "2FA"                  : "OFF",     # Keep this OFF if your 2FA isn't used
+        "Server"               : 1,         # 1=999proxy / 2=999dice / 3=999doge
+        "Reset If Profit"      : 0.00000001,
+        "Reset If Lose"        : 0,
+        "Reset If Win Streak"  : 1,
+        "Reset If Lose Streak" : 0,
+
+        "Target Balance"       : 250,
+
+        # Delay if basebet larger than 'X'
+        "If Base Bet"          :{
+                            "Amount": 0,
+                            "Delay" : 0    # 0 == OFF
+                            }
+        }
+
+Manual  ={
+
+        "Base Bet"     : 0.000014,
+        "Max Base Bet" : 0,
+        "Chance"       :{
+                        "Min": 49,
+                        "Max": 65
+                        },
+
+        "HiLo"         : "AUTO",  # AUTO / H / L
+
+        "If Win"       : 1,
+        "If Lose"      : 2.7,
+        "Number Bets"  : 0,    # Before excecuting Jump Set
+        "Jump Status"  : "ON",
+        "Jump Set"     :[
+                {
+                "Comment"       : "boom",
+                "Status"        : "on",
+                "Base Bet"      : 0.00001,
+                "Repeat Bets"   : 3,
+                "Chance"  :{
+                           "Min": 60,
+                           "Max": 64
+                           }
+                },{
+                "Comment"       : "tuyul",
+                "Status"        : "on",
+                "Base Bet"      : 0.000035,
+                "Repeat Bets"   : 5,
+                "Chance"  :{
+                           "Min": 54,
+                           "Max": 59
+                           }
+                },{
+                "xComment"       : "semvak",
+                "Status"        : "on",
+                "Base Bet"      : 0.00007,
+                "Repeat Bets"   : 10,
+                "Chance"  :{
+                           "Min": 49,
+                           "Max": 53
+                           }
+                }
+            ]
+        }
+
+ClientSeed ={
+            "Seed"           : 37837995,
+            "Auto"           : "on",
+            "If Win Streak"  : 1,
+            "If Lose Streak" : 9
+            }
+
+Extra   ={
+        "Target Profit"        : 3.78,
+        "Target Lose"          : 0,
+
+        "Action"               : "Relogin",  # Sleep or ReLogin
+        "Delay"                : 3
+        }
+
+#•••••••••••••••••••••••••••••••••••••••••••••••
+#
+#
+#
+#  BL`378_project
+#  --
+#
+#  #bancetz.Laut_
+#  https://t.me/ballataxart     [tg]
+#  https://t.me/Taraje_dah      [tg - group]
+#  https://t.me/taraje_channel  [tg - channel]
+#  378bancetz.laut@gmail.com
+#•••••••••••••••••••••••••••••••••••••••••••••••
