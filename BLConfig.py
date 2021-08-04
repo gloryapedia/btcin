2b79409b8fa2d8b5d8e411b9d4a6fee961274a27
gloryapedia <85855318+gloryapedia@users.noreply.github.com>
2021-08-04 21:06:30 +0700

    Add files via upload

--- BLConfig.py
+++ BLConfig.py
@@ -1,67 +1,230 @@
 # Cek sampai bawah!!
 Basic   ={
-        "Username"             : "pornhub",
-        "Password"             : "xnxx",
+        "Username"             : "sikuyuz",
+        "Password"             : "mahal",
         "BL-Key"               : "xxxxxxxxx",
 
-        "2FA"                  : "OFF",     # Keep this OFF if your 2FA isn't used
-        "Server"               : 1,         # 1=999proxy / 2=999dice / 3=999doge
+        # "2FA"                  : "OFF",     # Keep this OFF if your 2FA isn't used
+        "Server"               : 2,         # 1=999proxy / 2=999dice / 3=999doge
+        "Currency"             : 2,         # 2=DOGE / 3=LTC
         "Reset If Profit"      : 0.00000001,
         "Reset If Lose"        : 0,
-        "Reset If Win Streak"  : 1,
+        "Reset If Win Streak"  : 6,
         "Reset If Lose Streak" : 0,
 
-        "Target Balance"       : 10,
+        "Target Balance"       : 15,
 
         # Delay if basebet larger than 'X'
         "If Base Bet"          :{
-                            "Amount": 0,
+                            "Amount": 0.01,
                             "Delay" : 0    # 0 == OFF
                             }
         }
 
 Manual  ={
 
-        "Base Bet"     : 0.0000014,
+        "Base Bet"     : 0.001,
         "Max Base Bet" : 0,
         "Chance"       :{
-                        "Min": 11,
-                        "Max": 11
+                        "Min": 76,
+                        "Max": 87
                         },
 
-        "HiLo"         : "L",  # AUTO / H / L
+        "HiLo"         : "AUTO",  # AUTO / H / L
 
-        "If Win"       : 1,
-        "If Lose"      : 1.144,
-        "Number Bets"  : 10,    # Before excecuting Jump Set
-        "Jump Status"  : "ON",
+        "If Win"       : 1.2,
+        "If Lose"      : 1.1,
+        "Number Bets"  : 0,    # Before excecuting Jump Set
+        "Jump Status"  : "OFF",
         "Jump Set"     :[
                 {
-                "Comment"       : "boom",
+                "Comment"       : "1",
                 "Status"        : "on",
-                "Base Bet"      : 0.000006,
-                "Repeat Bets"   : 9,
+                "Base Bet"      : 0.01,
+                "Repeat Bets"   : 7,
                 "Chance"  :{
-                           "Min": 6,
-                           "Max": 8
+                           "Min": 33,
+                           "Max": 33
                            }
                 },{
-                "Comment"       : "tuyul",
+                "Comment"       : "2",
                 "Status"        : "on",
-                "Base Bet"      : 0.0000120,
-                "Repeat Bets"   : 9,
+                "Base Bet"      : 0.03,
+                "Repeat Bets"   : 7,
                 "Chance"  :{
-                           "Min": 6,
-                           "Max": 8
+                           "Min": 33,
+                           "Max": 33
                            }
                 },{
-                "xComment"       : "semvak",
+                "Comment"       : "3",
                 "Status"        : "on",
-                "Base Bet"      : 0.0000240,
-                "Repeat Bets"   : 9,
+                "Base Bet"      : 0.05,
+                "Repeat Bets"   : 7,
                 "Chance"  :{
-                           "Min": 6,
-                           "Max": 8
+                           "Min": 33,
+                           "Max": 33
+                           }
+                },{
+                "Comment"       : "4",
+                "Status"        : "on",
+                "Base Bet"      : 0.07,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 33,
+                           "Max": 33
+                           }
+                   },{
+                "Comment"       : "5",
+                "Status"        : "on",
+                "Base Bet"      : 0.08,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 33,
+                           "Max": 33
+                           }
+                },{
+                "Comment"       : "6",
+                "Status"        : "on",
+                "Base Bet"      : 0.0001,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                  },{
+                "Comment"       : "7",
+                "Status"        : "on",
+                "Base Bet"      : 0.0007,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                   },{
+                "Comment"       : "8",
+                "Status"        : "on",
+                "Base Bet"      : 0.0008,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                  },{
+                "Comment"       : "9",
+                "Status"        : "on",
+                "Base Bet"      : 0.0009,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                  },{
+                "Comment"       : "10",
+                "Status"        : "on",
+                "Base Bet"      : 0.001,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                 },{
+                "Comment"       : "11",
+                "Status"        : "on",
+                "Base Bet"      : 0.0019,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                 },{
+                "Comment"       : "12",
+                "Status"        : "on",
+                "Base Bet"      : 0.003,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                          }
+                 },{
+                "Comment"       : "13",
+                "Status"        : "on",
+                "Base Bet"      : 0.004,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                          }
+                 },{
+                "Comment"       : "14",
+                "Status"        : "on",
+                "Base Bet"      : 0.005,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                 },{
+                "Comment"       : "15",
+                "Status"        : "on",
+                "Base Bet"      : 0.006,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                  },{
+                "Comment"       : "16",
+                "Status"        : "on",
+                "Base Bet"      : 0.008,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                  },{
+                "Comment"       : "17",
+                "Status"        : "on",
+                "Base Bet"      : 0.0095,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                 },{
+                "Comment"       : "18",
+                "Status"        : "on",
+                "Base Bet"      : 0.012,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                 },{
+                "Comment"       : "19",
+                "Status"        : "on",
+                "Base Bet"      : 0.02,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                  },{
+                "Comment"       : "20",
+                "Status"        : "on",
+                "Base Bet"      : 0.033,
+                "Repeat Bets"   : 7,
+                "Chance"  :{
+                           "Min": 49,
+                           "Max": 49
+                           }
+                  },{
+                "Comment"       : "21",
+                "Status"        : "on",
+                "Base Bet"      : 0.043,
+                "Repeat Bets"   : 5,
+                "Chance"  :{
+                           "Min": 49,
+                           "Max": 49
                            }
                 }
             ]
@@ -75,11 +238,11 @@ ClientSeed ={
             }
 
 Extra   ={
-        "Target Profit"        : 1.5,
+        "Target Profit"        : 5,
         "Target Lose"          : 0,
 
         "Action"               : "Relogin",  # Sleep or ReLogin
-        "Delay"                : 300
+        "Delay"                : 3
         }
 
 #•••••••••••••••••••••••••••••••••••••••••••••••
